2764631cfbdd589b2f63e4ce507eaae0da708719
zhangfeixiong597-beep <zhangfeixiong597@gmail.com>
2026-01-03 14:14:16 +0800

    Update functions.php

--- oceanwp-child/functions.php
+++ oceanwp-child/functions.php
@@ -1,13 +1,24 @@
 <?php
-function duanwei_child_enqueue_styles() {
+/**
+ * OceanWP Child Theme Functions
+ *
+ * 确保代码全是英文符号，不要用中文符号。
+ */
+
+function oceanwp_child_enqueue_styles() {
+    // 父主题的句柄 (Handle)
     $parent_style = 'oceanwp-style';
 
+    // 1. 加载父主题样式
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 
+    // 2. 加载子主题样式 (确保在父主题之后加载，这样你的 CSS 才能覆盖父主题)
     wp_enqueue_style( 'oceanwp-child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'duanwei_child_enqueue_styles' );
+
+// 挂载到 WordPress 的脚本加载钩子上
+add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_styles' );
